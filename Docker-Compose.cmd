15702e8065e1227ab624f23556c58ffdd85e78d4
hot.delek@gmail.com <3e3e3e#E>
2018-12-29 23:02:28 +0200

    chaged docker script

--- Docker-Compose.cmd
+++ Docker-Compose.cmd
@@ -1,3 +1,3 @@
 PowerShell.exe -Command Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
 cd C:\DockerRedisPython
-PowerShell.exe -Command "& 'docker-compose -f docker-compose.yml up -d'"
+docker-compose -f docker-compose.yml up -d
