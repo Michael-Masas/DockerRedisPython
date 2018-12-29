d3f10a6b4b0bc5fe30d2ea07b63e604eadfcf2ef
hot.delek@gmail.com <3e3e3e#E>
2018-12-29 22:04:39 +0200

    Added Files for running the project

---
+++ Docker-Compose.ps1
@@ -0,0 +1,2 @@
+Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+docker-compose -f .\docker-compose.yml up -d
---
+++ selenium_test.py
@@ -0,0 +1,8 @@
+import time
+import selenium
+from selenium import webdriver                          ##Import Web Driver Module for Web Automation
+from selenium.webdriver.common.keys import Keys         ##Import Send Keys Module from selenium library
+
+driver = webdriver.Chrome(executable_path='C:\ci_pipeline\chromedriver.exe')                                       #Define Web Driver Path
+driver.get("http://localhost:5000")
+time.sleep(30)
