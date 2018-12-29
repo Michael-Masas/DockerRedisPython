17ca338e062ec30f2f151e6361bdb51e1452812e
hot.delek@gmail.com <3e3e3e#E>
2018-12-29 22:38:29 +0200

    deleted ps1 and added cmd file

--- Docker-Compose.cmd
+++ Docker-Compose.cmd
@@ -1,3 +1,3 @@
-Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+PowerShell.exe -Command Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
 cd C:\DockerRedisPython
 PowerShell.exe -Command "& 'docker-compose -f docker-compose.yml up -d'"
