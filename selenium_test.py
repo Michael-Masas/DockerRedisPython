dd80b2c4f1f452c2ff457c9e0e0085a53a6070a5
hot.delek@gmail.com <3e3e3e#E>
2018-12-29 22:18:28 +0200

    added driver quit to selenium test

--- selenium_test.py
+++ selenium_test.py
@@ -3,7 +3,7 @@ import selenium
 from selenium import webdriver                          ##Import Web Driver Module for Web Automation
 from selenium.webdriver.common.keys import Keys         ##Import Send Keys Module from selenium library
 
-driver = webdriver.Chrome(executable_path='C:\ci_pipeline\chromedriver.exe')                                       #Define Web Driver Path
+driver = webdriver.Chrome(executable_path='C:\DockerRedisPython\chromedriver.exe')                                       #Define Web Driver Path
 driver.get("http://127.0.0.1:5000")
 time.sleep(30)
 
